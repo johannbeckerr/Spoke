1b5a0c457f8ba28dc144ccc90f1cf9fa2aadbe73
Johann Becker <johann.drums@gmail.com>
2026-07-07 23:34:29 +0100

    alter corsConfig.java

--- spoke-api/src/main/java/com/spoke/config/CorsConfig.java
+++ spoke-api/src/main/java/com/spoke/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5173", "https://spoke-inky.vercel.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
